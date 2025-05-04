a696f7546ed57e8bb366ff450c1a28d4bc356b1a
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-05-04 23:17:29 +0800

    Refactor page layout and improve import statement for React in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 
 import Fab from './components/Fab';
 
 export default function Home(): ReactElement {
     return (
         <div className="relative min-h-screen">
+            <div className="m-4 mx-auto flex h-[297mm] w-[210mm] flex-col items-center justify-center border border-black">
+                <h1>A4 Size Block</h1>
+                <p>This is a vertical A4-sized block.</p>
+            </div>
             <Fab />
         </div>
     );
