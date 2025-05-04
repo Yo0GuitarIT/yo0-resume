a2a77d99485e733a060a356f2d413f8d4df926cf
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-05-04 15:46:12 +0800

    Add Prettier and ESLint configuration with Tailwind CSS support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,41 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals"),
-    {
-        plugins: ["import"],
-        rules: {
-            "no-unused-vars": [
-                "warn",
-                { vars: "all", args: "after-used", ignoreRestSiblings: false },
-            ],
-            "import/order": [
-                "warn",
-                {
-                    groups: [
-                        "builtin",
-                        "external",
-                        "internal",
-                        "parent",
-                        "sibling",
-                        "index",
-                    ],
-                    "newlines-between": "always",
-                    alphabetize: { order: "asc", caseInsensitive: true },
-                },
-            ],
+  ...compat.extends('next/core-web-vitals'),
+  {
+    plugins: ['import', 'prettier'],
+    rules: {
+      'no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+      ],
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
         },
+      ],
+      'prettier/prettier': 'warn',
     },
+  },
 ];
 
 export default eslintConfig;
