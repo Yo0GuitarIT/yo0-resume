7d0511f4f8a23ee4f6d7e2fedb85f3ce3dd3bc49
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-05-04 15:41:16 +0800

    Refactor ESLint configuration, remove jsconfig.json, and update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals"),
+    {
+        plugins: ["import"],
+        rules: {
+            "no-unused-vars": [
+                "warn",
+                { vars: "all", args: "after-used", ignoreRestSiblings: false },
+            ],
+            "import/order": [
+                "warn",
+                {
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index",
+                    ],
+                    "newlines-between": "always",
+                    alphabetize: { order: "asc", caseInsensitive: true },
+                },
+            ],
+        },
+    },
+];
 
 export default eslintConfig;
